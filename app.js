35126a8a0dd56e17d06b2f64e9e7b43b54a26adf
Ivan HA <hangolam@gmail.com>
2017-04-13 22:00:30 +0800

    add nodejs file template

---
+++ app.js
@@ -0,0 +1,21 @@
+fs = require('fs');
+
+let file = 'in.txt';
+
+fs.readFile(file, 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+
+    data = data.split('\n');
+    // console.log(data);
+
+    for (let i = 1; i < data[0] + 1 && data[i]; i++) {
+        let result = calc(data[i]);
+        console.log(`Case #${i}: ${result}`);
+    }
+});
+
+function calc(str) {
+
+}
